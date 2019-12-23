c85bdc46f73dce8c65cad55523aa2b45c3f7c1a9
Joseph Caero <joeycaero@gmail.com>
2019-12-23 13:23:50 -0600

    update assembly info, improve error message

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using dtimer;
 using System;
+using System.Configuration;
 using System.Windows;
 
 namespace DTimer
@@ -11,6 +12,18 @@ namespace DTimer
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            //the exe.config stores our default settings, we want our app to work
+            //if the exe.config doesn't exist so we do a try catch here
+            try
+            {
+                var endTime = saved.Default.GoalEndTime;
+            }catch (NullReferenceException)
+            {
+                string settingsLocation = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath;
+                MessageBox.Show("dtimer.exe requires a time to be set, no save data found in " + settingsLocation);
+                Current.Shutdown();
+            }
+
             if (e.Args.Length == 1)
             {
                 saved.Default.GoalEndTime = DateTime.Now.AddDays(double.Parse(e.Args[0]));
